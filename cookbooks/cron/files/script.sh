0a8801e8b5b6ec5a7e72821caa4e633bf51890e6
tmoshe <64310013+tmoshe@users.noreply.github.com>
2023-06-25 18:39:39 +0300

    Update script.sh

--- cookbooks/cron/files/script.sh
+++ cookbooks/cron/files/script.sh
@@ -3,7 +3,13 @@
 # This is a sample Bash script
 # It prints a welcome message and current date
 
+sudo su
 echo "Welcome to the script!" >> /home/ubuntu/file2.txt
 echo "Today is $(date)" >> /home/ubuntu/file2.txt
-git pull https://github.com/tmoshe/chef.git
+cd /home/ubuntu
+git clone https://github.com/tmoshe/chef.git
 chef-solo -c /home/ubuntu/chef/solo.rb -j /home/ubuntu/chef/runlist.json --chef-license=accept
+
+                                                                   
+
+
