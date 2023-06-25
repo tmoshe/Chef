6432ed30bf7d8f43c7cf68df594abd8f9de2ab07
tmoshe <64310013+tmoshe@users.noreply.github.com>
2023-06-25 19:11:10 +0300

    Update script.sh

--- cookbooks/cron/files/script.sh
+++ cookbooks/cron/files/script.sh
@@ -7,7 +7,9 @@ sudo su
 echo "Welcome to the script!" >> /home/ubuntu/file2.txt
 echo "Today is $(date)" >> /home/ubuntu/file2.txt
 cd /home/ubuntu/chef
-git clone https://github.com/tmoshe/chef.git
+#git remote add origin https://github.com/tmoshe/chef.git
+git pull origin main
+#git clone https://github.com/tmoshe/chef.git
 chef-solo -c /home/ubuntu/chef/solo.rb -j /home/ubuntu/chef/runlist.json --chef-license=accept
 
                                                                    
