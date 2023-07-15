33f0202246e6b9db9e3090533cfd61be6ad498b5
tmoshe <64310013+tmoshe@users.noreply.github.com>
2023-07-15 12:12:05 +0300

    Update default.rb

--- cookbooks/s3/recipes/default.rb
+++ cookbooks/s3/recipes/default.rb
@@ -24,11 +24,10 @@ end
 version = node["s3"]["ver"]
 
 
-remote_file_s3 "/home/ubuntu/app/my-app-#{version}.tar" do 
-  bucket 'cloudschoolartifacts'
+remote_file_s3 "/home/ubuntu/app/my-app-#{version}.tar" do
+#  bucket 'cloudschoolartifacts'
+  bucket 'bucketcloudschool'
   remote_path "my-app-#{version}.tar"
-  aws_access_key_id 'AKIASF5WYYCAZFLVH755'
-  aws_secret_access_key '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'
   region 'eu-west-1'
   action :create
 end
@@ -37,6 +36,32 @@ execute 'extract_artifact' do
   command "tar -xf /home/ubuntu/app/my-app-#{version}.tar -C /home/ubuntu/app"
   action :run
 end
+
+
+
+#directory '/home/ubuntu/app' do
+#  owner 'root'
+#  group 'root'
+#  mode '0777'
+#  action :create
+#end
+
+#version = node["s3"]["ver"]
+
+
+#remote_file_s3 "/home/ubuntu/app/my-app-#{version}.tar" do 
+#  bucket 'cloudschoolartifacts'
+#  remote_path "my-app-#{version}.tar"
+#  aws_access_key_id 'AKIASF5WYYCAZFLVH755'
+#  aws_secret_access_key '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'
+#  region 'eu-west-1'
+#  action :create
+#end
+
+#execute 'extract_artifact' do
+#  command "tar -xf /home/ubuntu/app/my-app-#{version}.tar -C /home/ubuntu/app"
+#  action :run
+#end
 
 #remote_file_s3 '/home/ubuntu/app/my-app-19.tar' do
 #  bucket 'cloudschoolartifacts'
