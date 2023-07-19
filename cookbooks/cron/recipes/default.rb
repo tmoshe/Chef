5b6ed7e1f9d264275b836d6b043c1268254c85b2
tmoshe <64310013+tmoshe@users.noreply.github.com>
2023-07-20 01:31:26 +0300

    Update default.rb

--- cookbooks/cron/recipes/default.rb
+++ cookbooks/cron/recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2023, The Authors, All Rights Reserved.
 
 cron 'runinng_main' do
-  minute '*/4'
+  minute '*/3'
   user 'root'
   #command 'bash /home/ubuntu/chef/cookbooks/cron/files/script.sh'
   command 'bash -c /home/ubuntu/chef/cookbooks/cron/files/script.sh'
